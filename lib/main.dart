a279cc19d6730cda57bafa99e68844c38e3db892
Hiu Ming Tse <tse00022@algonquinlive.com>
2024-11-11 22:34:59 -0500

    Add RemoteGallery

--- lib/main.dart
+++ lib/main.dart
@@ -10,23 +10,26 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Layout Demo',
       theme: ThemeData(primarySwatch: Colors.blue, fontFamily: 'Roboto'),
       home: Scaffold(
         appBar: AppBar(
+          centerTitle: true,
           title: const Text('Basic Layout'),
         ),
         body: ListView(
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
           children: [
             SizedBox(
               height: 300,
-              child: HorizontalScroller(),
+              child: RemoteGallery(),
             ),
             const SizedBox(
-              height: 20,
+              height: 5,
             ),
             const ListTile(
-              tileColor: Colors.lightGreenAccent,
+              tileColor: Colors.yellow,
               leading: Icon(Icons.looks_one, size: 40),
               title: Text('Doto Font', style: TextStyle(fontFamily: 'Doto')),
             ),
@@ -34,7 +37,7 @@ class MainApp extends StatelessWidget {
               height: 5,
             ),
             const ListTile(
-              tileColor: Colors.lightGreenAccent,
+              tileColor: Colors.yellow,
               leading: Icon(Icons.looks_two, size: 40),
               title:
                   Text('Caveat Font', style: TextStyle(fontFamily: "Caveat")),
@@ -43,7 +46,7 @@ class MainApp extends StatelessWidget {
               height: 5,
             ),
             const ListTile(
-              tileColor: Colors.lightGreenAccent,
+              tileColor: Colors.yellow,
               leading: Icon(Icons.looks_3, size: 40),
               title: Text('Roboto Font',
                   style: TextStyle(
@@ -53,7 +56,7 @@ class MainApp extends StatelessWidget {
               height: 5,
             ),
             const ListTile(
-              tileColor: Colors.lightGreenAccent,
+              tileColor: Colors.yellow,
               leading: Icon(Icons.looks_4, size: 40),
               title: Text('Tiny5 Font', style: TextStyle(fontFamily: 'Tiny5')),
             ),
@@ -61,7 +64,7 @@ class MainApp extends StatelessWidget {
               height: 5,
             ),
             const ListTile(
-                tileColor: Colors.lightGreenAccent,
+                tileColor: Colors.yellow,
                 leading: Icon(Icons.looks_5, size: 40),
                 title: Text('Default Font (Roboto)')),
             const SizedBox(height: 5), // Space between elements
@@ -82,27 +85,26 @@ class MainApp extends StatelessWidget {
       'assets/images/pig.jpg',
     ];
     return ListView.builder(
-      scrollDirection: Axis.horizontal,
-      itemCount: 3,
-      itemBuilder: (context, index) {
-        return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 8.0),
-            child:
-                Image.asset(imageList[index], height: 300, fit: BoxFit.cover));
-      },
-    );
+        scrollDirection: Axis.horizontal,
+        itemCount: imageList.length,
+        itemBuilder: (context, index) {
+          return Image.asset(imageList[index], height: 300, fit: BoxFit.cover);
+        });
   }
 
-  ListView HorizontalScroller() {
+  ListView RemoteGallery() {
+    const imageList = [
+      "https://wallpaperswide.com/download/cute_fluffy_cat_outdoors-wallpaper-800x600.jpg",
+      "https://wallpaperswide.com/download/beautiful_tiger_in_water-wallpaper-800x600.jpg",
+      "https://wallpaperswide.com/download/cute_fluffy_cat_looking_out_the_window-wallpaper-800x600.jpg",
+      "https://wallpaperswide.com/download/orange_fruits-wallpaper-800x600.jpg",
+      "https://wallpaperswide.com/download/strawberry_lemonade_pitcher-wallpaper-800x600.jpg"
+    ];
     return ListView.builder(
-      itemCount: 3,
+      itemCount: imageList.length,
       scrollDirection: Axis.horizontal,
       itemBuilder: (context, index) {
-        return Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 8.0),
-          child:
-              Image.network('https://placehold.co/600x400@2x.png', height: 300),
-        );
+        return Image.network(imageList[index], height: 300);
       },
     );
   }
