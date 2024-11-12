46ca6bbe8abbcf27a41ba9cb74daa7c7d125b4d6
Hiu Ming Tse <tse00022@algonquinlive.com>
2024-11-11 19:32:16 -0500

    Add widget HorizontalGallery

--- lib/main.dart
+++ lib/main.dart
@@ -20,51 +20,61 @@ class MainApp extends StatelessWidget {
         body: Padding(
           padding: const EdgeInsets.all(8),
           child: ListView(
-            children: [
+            children: const [
+              HorizontalGallery(),
               SizedBox(
-                height: 400,
-                child: ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: 3,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: Image.network(
-                          'https://placehold.co/600x400@2x.png',
-                          width: 200,
-                          height: 200),
-                    );
-                  },
-                ),
-              ),
-              const SizedBox(
                 height: 20,
               ),
-              const ListTile(
+              ListTile(
                 leading: Icon(Icons.star),
                 title: Text('ListTile with Regular Font',
                     style: TextStyle(fontFamily: 'Doto')),
               ),
-              const ListTile(
+              ListTile(
                 leading: CircleAvatar(child: Icon(Icons.person)),
                 title: Text('ListTile with Bold Font',
                     style: TextStyle(
                         fontFamily: 'Doto', fontWeight: FontWeight.bold)),
               ),
-              const ListTile(
+              ListTile(
                 leading: Icon(Icons.settings),
                 title: Text('ListTile with Italic Font',
                     style: TextStyle(
                         fontFamily: 'Doto', fontStyle: FontStyle.italic)),
               ),
-              const ListTile(
+              ListTile(
                 trailing: Icon(Icons.arrow_forward),
                 title: Text('Default Device Font'),
               ),
-              const SizedBox(height: 20), // Space between elements
+              SizedBox(height: 20), // Space between elements
+              HorizontalGallery()
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class HorizontalGallery extends StatelessWidget {
+  const HorizontalGallery({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 200,
+      child: ListView.builder(
+        scrollDirection: Axis.horizontal,
+        itemCount: 3,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Image.network('https://placehold.co/600x400@2x.png',
+                width: 200, height: 200),
+          );
+        },
       ),
     );
   }
