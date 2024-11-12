c58dd39124ca68da6f50f94debadc327cff9fb62
Hiu Ming Tse <tse00022@algonquinlive.com>
2024-11-11 20:18:06 -0500

    Add ListTiles

--- lib/main.dart
+++ lib/main.dart
@@ -26,25 +26,42 @@ class MainApp extends StatelessWidget {
               height: 20,
             ),
             const ListTile(
+              tileColor: Colors.lightGreenAccent,
               leading: Icon(Icons.looks_one, size: 40),
               title: Text('Doto Font', style: TextStyle(fontFamily: 'Doto')),
             ),
+            const SizedBox(
+              height: 5,
+            ),
             const ListTile(
+              tileColor: Colors.lightGreenAccent,
               leading: Icon(Icons.looks_two, size: 40),
               title:
                   Text('Caveat Font', style: TextStyle(fontFamily: "Caveat")),
             ),
+            const SizedBox(
+              height: 5,
+            ),
             const ListTile(
+              tileColor: Colors.lightGreenAccent,
               leading: Icon(Icons.looks_3, size: 40),
               title: Text('Roboto Font',
                   style: TextStyle(
                       fontFamily: 'Roboto', fontStyle: FontStyle.normal)),
             ),
+            const SizedBox(
+              height: 5,
+            ),
             const ListTile(
+              tileColor: Colors.lightGreenAccent,
               leading: Icon(Icons.looks_4, size: 40),
               title: Text('Tiny5 Font', style: TextStyle(fontFamily: 'Tiny5')),
             ),
+            const SizedBox(
+              height: 5,
+            ),
             const ListTile(
+                tileColor: Colors.lightGreenAccent,
                 leading: Icon(Icons.looks_5, size: 40),
                 title: Text('Default Font (Roboto)')),
             const SizedBox(height: 20), // Space between elements
